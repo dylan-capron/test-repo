80389943f3240800b1e669ac3ce84f70a7dde475
Dylan Capron <146707495+dylan-capron@users.noreply.github.com>
2025-08-05 11:56:15 +0200

    Simule un échec contrôlé dans check_app.sh

--- app/check_app.sh
+++ app/check_app.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-echo "Début des vérifications…"
-if [ -f app/index.html ]; then
-  echo "Le fichier index.html est présent. Test réussi."
-  echo "Fin des vérifications."
-  exit 0
-else
-  echo "Le fichier index.html est absent !"
-  echo "Fin des vérifications."
-  exit 1
-fi
+echo "Test d'échec simulé"
+exit 1
